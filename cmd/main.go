771fcd1f7f97dfc891958246ca448f069587a59d
Gutto <guttolaudie@gmail.com>
2025-02-12 13:40:51 -0300

    feat: add models.go with db tables
    added internal/models/models.go and automigration in main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"backend-picpay/internal/models"
+	"backend-picpay/internal/utils"
+
 	"log"
 )
 
 func main() {
-	log.Println("In construction")
+	db := utils.ConnectDB()
+	if db == nil {
+		log.Fatalf("Failed to connect to database")
+	}
+
+	err := db.AutoMigrate(&models.User{}, &models.Wallet{}, &models.Transaction{}, &models.Notification{})
+	if err != nil {
+		log.Fatalf("Failed to migrate database:  %v", err)
+	}
+
+	log.Println("Database migration completed succesfully")
 }
---
+++ internal/models/models.go
@@ -0,0 +1,39 @@
+package models
+
+import (
+	"time"
+)
+
+type User struct {
+	ID			uint		`gorm:"primaryKey"`
+	Name		string		`gorm:"not null"`
+	Document	string		`gorm:"unique;not null"`
+	Email		string		`gorm:"unique;not null"`
+	Password	string		`gorm:"not null"`
+	UserType	string		`gorm:"not null"`
+	CreatedAt	time.Time	`gorm:"default:CURRENT_TIMESTAMP"`
+}
+
+type Wallet struct {
+	ID			uint		`gorm:"primaryKey"`
+	UserID		uint		`gorm:"not null"`
+	Balance		float64		`gorm:"not null"`
+	CreatedAt	time.Time	`gorm:"default:CURRENT_TIMESTAMP"`
+}
+
+type Transaction struct {
+	ID			uint		`gorm:"primaryKey"`
+	Value		float64		`gorm:"not null"`
+	PayeerID	uint		`gorm:"not null"`
+	PayeeID		uint		`gorm:"not null"`
+	CreatedAt	time.Time	`gorm:"default:CURRENT_TIMESTAMP"`
+}
+
+type Notification struct {
+	ID				uint		`gorm:"primaryKey"`
+	UserID			string		`gorm:"not null"`
+	TransactionID	string		`gorm:"not null"`
+	Message			string		`gorm:"not null"`
+	Status			string		`gorm:"not null"`
+	CreatedAt		time.Time	`gorm:"default:CURRENT_TIMESTAMP"`
+}
